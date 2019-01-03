3b62d5cc847fe2d2b20a1c61282ec0025d954632
rakeshmadivi <44462799+rakeshmadivi@users.noreply.github.com>
2019-01-03 13:37:42 +0530

    Update tweaks.sh

--- tweaks.sh
+++ tweaks.sh
@@ -10,3 +10,6 @@ install_cpupower()
 
 # Syblings list using node ids and cpu number
 echo $(for i in 0 1 2 3; do for j in `ls /sys/devices/system/node/node${i}/| grep cpu |head -n 6`;do cat /sys/devices/system/node/node${i}/${j}/topology/thread_siblings_list|cut -f1 -d",";done; done;)
+
+
+jbb=https://drive.google.com/open?id=1wr2wIFkbj_pCqUL-GcoPxtPCuRsm938E
