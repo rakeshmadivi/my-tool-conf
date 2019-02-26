e969e13e598514f0319b6df8bb7b7ca9688981d0
rakeshmadivi <44462799+rakeshmadivi@users.noreply.github.com>
2019-02-26 18:02:01 +0530

    Update useful_linux_cmds.sh

--- useful_linux_cmds.sh
+++ useful_linux_cmds.sh
@@ -20,3 +20,23 @@ sudo systemctl isolate runlevel3.target
 
 # Check run level using 'who' command
 who -r
+
+
+# NVME DEVICE RELATED COMMANDS
+: '
+nvme --version NVMe CLI version
+ nvme help List of all commands
+ sudo nvme list Lists out nvme devices, capacity, namespace, format and FW rev
+ sudo nvme format /dev/nvme0 –s 1 Format with secure erase option
+ sudo nvme id-ctrl /dev/nvme0 Identify controller details
+ sudo nvme intel id-ctrl /dev/nvme0n1 Identify controller details - few additional details added (e.g. drive
+health, boot loader)
+ sudo nvme intel market-name /dev/nvme0n1 Marketing series name
+ sudo nvme id-ns /dev/nvme0n1 Identify namespace details
+ sudo nvme get-log /dev/nvme0 --log-id=2 --log-len=512 Read log page
+ sudo nvme smart-log /dev/nvme0 Smart log
+ sudo nvme error-log /dev/nvme0 | less Shows error logs
+ sudo nvme fw-download /dev/nvme0n1 -f <fw binary> Expected result: Firmware download success
+ sudo nvme fw-commit /dev/nvme0n1 -s 1 -a 1 Expected result: Success activating firmware action:1 slot:1
+ sudo nvme reset /dev/nvme0 Resets the nvme controller
+ '
