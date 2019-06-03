6f45fa5b5eec21878595e1c7d384592e0bdb9603
rakeshmadivi <44462799+rakeshmadivi@users.noreply.github.com>
2019-06-03 17:31:14 +0530

    Update useful_linux_cmds.sh
    Added cmd to get grub menu to boot into kernel

--- useful_linux_cmds.sh
+++ useful_linux_cmds.sh
@@ -43,3 +43,6 @@ health, boot loader)
 # commands related to devices
 dd if=/dev/zero of=${HOME}/dd_generated.img
 blockdev --getbsz /dev/zero # To get block size of device
+
+# GET GRUB Menu Etry
+awk -F\' '/menuentry / {print $2}' /boot/grub/grub.cfg 
